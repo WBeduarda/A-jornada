f1ff54814b8614ccf0a976418777cc506c13c7d2
WBeduarda <eduarda.rosa1208@gmail.com>
2024-09-14 18:45:29 -0300

    teste

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 const avanca = document.querySelectorAll(".btn-proximo");
 
 avanca.forEach(button => {
-    button.addEventListener("click", function() {
+    button.addEventListener("click", function(){
         const atual = document.querySelector(".ativo");
-        const proximoPasso = "passo-" + this.getAttribute("data-proximo");
+        const proximoPasso = "passo-" + this.getAttribute("data-proxima");
 
-        // Remove a classe 'ativo' do passo atual
         atual.classList.remove("ativo");
-
-        // Adiciona a classe 'ativo' ao próximo passo
         document.getElementById(proximoPasso).classList.add("ativo");
-    });
-});
+    
+    })
+})
