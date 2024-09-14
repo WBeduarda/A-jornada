add15fdc52ec3c9d9ad1d627a32f34abe6c44bfb
WBeduarda <eduarda.rosa1208@gmail.com>
2024-09-14 16:24:57 -0300

    pqr

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-const avanca = document.querySelectorAll()
+const avanca = document.querySelectorAll(".btn-proximo");
+
+avanca.forEach(button => {
+    button.addEventListener("click", function(){
+        const atual = document.querySelector(".ativo");
+        const proximoPasso = "passo-" + this.getAttribute("data-proxima");
+
+        atual.classList.remove("ativo");
+        document,getElement\By\IdleDeadline(proximoPasso).classList.add("ativo");
+    })
+})
